8fbec13fbaef27b31d5331e65d3050a9dcdd2a82
vitorino <117422820+lolalos@users.noreply.github.com>
2023-08-09 01:43:47 -0500

    App App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -17,11 +17,11 @@ const App = () => {
         },
         "&::-webkit-scrollbar-thumb": {
           boxShadow: "inset 0 0 6px rgba(0, 0, 0, 0.3)",
-          backgroundColor: "#e8e8e8",
+          backgroundColor: "#8cd65a",
         },
       },
       palette: {
-        primary: { main: "#2576d2" },
+        primary: { main: "#07c037" },
       },
     },
     locale
